b891f6402ac3ee87c5f8875307d231c568962390
Taoheed King <tking92@montgomerycollege.edu>
2020-04-14 20:11:45 -0400

    Add files via upload

---
+++ VariableProject/src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int test1 = 80;
+        double price1 = 45.99;
+        String userName = "Jake";
+
+        System.out.println("Test score initialized is: " + test1);
+        System.out.println("Price tag is: " + price1);
+        System.out.println("Username: " + userName);
+    }
+
+}
